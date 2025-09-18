4bef06d8e1791cdc73a337b849d3ac081f1493fe
Navaneeth krishnan S <navaneethkrishnans2000@gmail.com>
2025-09-18 22:20:54 +0530

    travel journal first commit

---
+++ travel-journal/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="src/images/globe.png" alt="Globe Icon" />
+            <h1>My travel journal.</h1>
+        </header>
+    )
+}

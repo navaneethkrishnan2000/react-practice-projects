3d3c9eab1c0721c188a387de81b080561854c9eb
Navaneeth <navaneethkrishnans2000@gmail.com>
2025-09-19 11:42:31 +0530

    .

--- travel-journal/src/App.jsx
+++ travel-journal/src/App.jsx
@@ -2,11 +2,24 @@ import Header  from "./components/Header"
 import Entry from "./components/Entry"
 
 export default function App() {
+
+  const hours = new Date().getHours();
+  let timeOfDay;
+
+  if(hours < 12) {
+    timeOfDay = "morning";
+  } else if (hours >=12 && hours < 17) {
+    timeOfDay = "afternoon";
+  } else {
+    timeOfDay = "night";
+  }
+
   return(
     <>
       <Header />
       <Entry />
       <h1>I'm the App component</h1>
+      <h1>{timeOfDay}</h1>
     </>
   )
 }

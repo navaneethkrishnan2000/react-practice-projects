d09046e4d34be3fcf199baab64e41df09dbb0ed3
Navaneeth <navaneethkrishnans2000@gmail.com>
2025-09-19 17:56:46 +0530

    completed mapping components

---
+++ mapping-components/src/App.jsx
@@ -0,0 +1,18 @@
+import Joke from "./components/.Joke";
+import jokesData from "./jokesData";
+
+export default function App() {
+    const jokeElements = jokesData.map((joke) => {
+        return <Joke 
+                setup = {joke.setup}
+                punchline = {joke.punchline}    
+            />
+    })
+
+    return(
+        <main>
+            {jokeElements}
+        </main>
+    )
+
+}
